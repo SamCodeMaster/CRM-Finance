60f2c689957c7d0e2967141c195d6062aab9d920
gio10herrera <herrerafontalvo@outlook.com>
2024-02-11 13:59:25 -0500

    creacion del componente login, aun falta crear el modelo de login y el servicio

--- actividad-2-front/src/app/components/login/login.component.ts
+++ actividad-2-front/src/app/components/login/login.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { ContenedorModule } from '../../contenedor/contenedor.module';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ContenedorModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  //public usuario: any;
+  //public contrasena: any;
+  public datosLogin: any;
 
+  constructor(){
+    //this.usuario = "";
+    //this.contrasena = "";
+    this.datosLogin = {
+      usuario: "",
+      contrasena: ""
+    }
+  }
+
+  onSubmit():void{
+    alert("formulario enviado");
+    console.log(this.datosLogin)
+  }
 }
