f5f7266ac053c94ed3fe1bc54cb7860242e3ac3c
gio10herrera <herrerafontalvo@outlook.com>
2024-03-25 17:34:24 -0500

    creacion de interfaz sidenav referenciacion

--- actividad-2-front/src/app/components/referenciacion/referenciacion.component.ts
+++ actividad-2-front/src/app/components/referenciacion/referenciacion.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-referenciacion',
   standalone: true,
-  imports: [],
+  imports: [MatSidenavModule, MatListModule, MatIconModule, RouterModule],
   templateUrl: './referenciacion.component.html',
   styleUrl: './referenciacion.component.css'
 })
